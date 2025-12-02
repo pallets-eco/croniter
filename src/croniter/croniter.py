ea7fd733a1dda50541039f2971d47a3868364fb4
Benjamin Drung <benjamin.drung@canonical.com>
2025-12-02 11:34:00 +0100

    remove obsolete noqa: E241 (#192)
    The `noqa: E241` marker are not needed any more after wrapping the lines
    (with ruff).

--- src/croniter/croniter.py
+++ src/croniter/croniter.py
@@ -65,11 +65,11 @@ M_ALPHAS: dict[str, Union[int, str]] = {
     "jan": 1,
     "feb": 2,
     "mar": 3,
-    "apr": 4,  # noqa: E241
+    "apr": 4,
     "may": 5,
     "jun": 6,
     "jul": 7,
-    "aug": 8,  # noqa: E241
+    "aug": 8,
     "sep": 9,
     "oct": 10,
     "nov": 11,
